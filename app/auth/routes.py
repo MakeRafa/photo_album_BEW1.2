40972e5121ca2af2d3304a45c128205e5377d2e5
MakeRafa <rafavazquez229@gmail.com>
2021-05-10 15:14:23 -0700

    created models and now making hmtl files

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -9,7 +9,7 @@ from app import app, db, bcrypt
 auth = Blueprint('auth', __name__)
 
 # Create your routes here.
-
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm
     if form.validate_on_submit():
@@ -18,3 +18,28 @@ def signup():
             username=form.username.data,
             password=hashed_password
         )
+        db.session.add()
+        db.session.commit()
+        flash('Account Has Been Created!')
+        return redirect(url_for('auth.login'))
+    return render_template('signup.html', form=form)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            next_page = request.args.get('next')
+            print('Account logged in!')
+            return redirect(next_page if next_page else url_for('main.homepage'))
+
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
--- app/models.py
+++ app/models.py
@@ -1,15 +1,30 @@
 from sqlalchemy_utils import URLType
 
+from sqlalchemy.orm import backref
+
 from flask_login import UserMixin
 
-from grocery_app import db
-from grocery_app.utils import FormEnum
+from app import db
+from app.utils import FormEnum
 # Create your models here.
 class User:
     """User Model"""
-    id = 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+
 class Album:
     """Album Model"""
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+
+    # photos = db.relationship()
 
 class Photo:
     """Photo Model"""
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    small_description = db.Column(db.String(200), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+
