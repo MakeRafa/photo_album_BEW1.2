bffd48865285f0f225972c91b804624823a28c26
MakeRafa <rafavazquez229@gmail.com>
2021-05-14 16:10:18 -0700

    everything but updating album is functioning, from there moving onto tests

--- photoalbum_app/main/forms.py
+++ photoalbum_app/main/forms.py
@@ -1,13 +1,14 @@
+from photoalbum_app.models import Album
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SelectField, SubmitField, FloatField, PasswordField
+from wtforms import StringField, DateField, SubmitField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Length, URL, ValidationError
+from wtforms.validators import DataRequired, Length, URL
 
 # Create your forms here.
 
 class AlbumForm(FlaskForm):
     """Album Form for created new Albums"""
-    title = StringField("Title", validators=[DataRequired(), Length(min=5, max=10)])
+    title = StringField("Title", validators=[DataRequired(), Length(min=5, max=20)])
     description = StringField("Description", validators=[DataRequired(), Length(min=5, max=200)])
     submit = SubmitField("Submit")
 
@@ -18,4 +19,5 @@ class PhotoForm(FlaskForm):
     location = StringField("Location", validators=[DataRequired(), Length(min=5, max=100)])
     date = DateField("Date", format= '%Y-%m-%d')
     small_description = StringField("Small Description", validators=[DataRequired(), Length(min=5, max=100)])
+    album = QuerySelectField('Album', query_factory=lambda: Album.query, allow_blank=False)
     submit = SubmitField("Submit")
--- photoalbum_app/main/routes.py
+++ photoalbum_app/main/routes.py
@@ -1,4 +1,6 @@
+from os import name
 from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_required
 
 from photoalbum_app import app, db
 
@@ -14,23 +16,25 @@ def homepage():
     return render_template('home.html', all_albums=all_albums)
 
 @main.route('/create_album', methods=['GET', 'POST'])
+@login_required
 def create_album():
     form = AlbumForm()
 
     if form.validate_on_submit():
-        # create new Album and direct user to add info
-        new_album = AlbumForm(
+        new_album = Album(
             title=form.title.data,
             description=form.description.data
         )
         db.session.add(new_album)
+        
         db.session.commit()
         flash('Your new album has been added!')
 
-        return redirect(url_for('main.album_info', album_id=new_album.id))
+        return redirect(url_for('main.homepage', album_id=new_album.id))
     return render_template('create_album.html', form=form)
 
 @main.route('/album/<album_id>', methods=['GET', 'POST'])
+@login_required
 def album_info(album_id):
     album = Album.query.get(album_id)
 
@@ -43,5 +47,47 @@ def album_info(album_id):
         db.session.commit()
         flash('Your Album has been updated!')
 
-        return redirect(url_for('main.album_info', album_id=album.id))        
+        return redirect(url_for('main.homepage', album_id=album.id))        
     return render_template('album_info.html', album=album, form=form)
+
+@main.route('/create_photo', methods=['GET', 'POST'])
+@login_required
+def create_photo():
+    form = PhotoForm()
+    if form.validate_on_submit():
+        new_photo = Photo(
+            name=form.name.data,
+            photo_url=form.photo_url.data,
+            location=form.location.data,
+            date=form.date.data,
+            album=form.album.data,
+            small_description=form.small_description.data
+        )
+        db.session.add(new_photo)
+        db.session.commit()
+
+        flash('Photo has been added to the designated album')
+        
+        return redirect(url_for('main.homepage', photo_id=new_photo.id))
+    return render_template('create_photo.html', form=form)
+
+@main.route('/photo/<photo_id>', methods=['POST', 'GET'])
+@login_required
+def photo_info(photo_id):
+    photo = Photo.query.get(photo_id)
+
+    form = PhotoForm(obj=photo)
+
+    if form.validate_on_submit():
+        photo.name = form.name.data
+        photo.photo_url = form.photo_url.data
+        photo.location = form.location.data
+        photo.date = form.date.data
+        photo.album = form.album.data
+        photo.small_description = form.small_description.data
+
+        db.session.commit()
+        flash('Your photo has been successfully updated')
+
+        return redirect(url_for('main.photo_info', photo_id=photo.id))        
+    return render_template('photo_info.html', photo=photo, form=form)
